3837099efcec47d940e8d440c6c510e1050a453f
Luke Todd <luketodd@zoho.com>
2019-02-24 18:31:00 +0000

    Using bloomer and bulma

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, Columns, Column } from 'bloomer';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import 'bulma';
+import './App.scss';
+
+function App() {
+  return (
+    <Container>
+      <Columns>
+        <Column isSize="1/3">navigation</Column>
+        <Column>calendar</Column>
+      </Columns>
+    </Container>
+  );
 }
 
 export default App;
