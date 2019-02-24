b085bf439b6ccb93e28d2d87a0bfc60a71145414
Luke Todd <luketodd@zoho.com>
2019-02-24 19:23:15 +0000

    Updated gitignore for firebase config and done basic events mapping

--- src/components/Calendar/CalendarWrapper.js
+++ src/components/Calendar/CalendarWrapper.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Button } from 'bloomer';
+
+import firebase from '../../firebase';
+import { useCollection } from 'react-firebase-hooks/firestore';
+
+const eventsDb = firebase.firestore().collection('events');
+
+const _addEvent = () => {
+  eventsDb.add({
+    end: null,
+    start: 'test',
+    title: 'test'
+  });
+};
 
 export default function CalendarWrapper() {
+  const { error, loading, value } = useCollection(eventsDb);
+
   return (
     <div className="calendar">
-      <div className="calendar__inner">...</div>
+      <div className="calendar__inner">
+        {value &&
+          value.docs.map(doc => (
+            <li key={doc.id}>{JSON.stringify(doc.data())}</li>
+          ))}
+      </div>
+      <Button onClick={_addEvent}>Add new</Button>
     </div>
   );
 }
